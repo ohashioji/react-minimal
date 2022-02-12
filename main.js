35a6beac54af9b3fd5706a732fad0f614b09689e
lockedNLevered <shaynel@sfu.ca>
2022-02-11 16:39:29 -0800

    project compiles and server can start

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 import chalk from "chalk";
 import commander from "commander";
 import fs from "fs-extra";
-import path from "path";
+import path, { resolve } from "path";
 import os from "os";
 import tmp from "tmp";
+import spawn from "cross-spawn";
+import { execSync } from "child_process";
+import templateJs from "./template/template.js";
 
 const HTML_BASE = `<!DOCTYPE html> <html lang='en'><head>
 		<meta charset='UTF-8' />
@@ -33,6 +36,58 @@ import React from "react";
 import ReactDom from "react-dom";
 ReactDom.render(<App />, document.getElementById("root"));
 `;
+
+const WEBPACK = `const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/index.tsx",
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				options: { presets: ["@babel/env"] },
+			},
+			{
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				loader: "ts-loader",
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+	resolve: { extensions: [".css", ".js", ".jsx", ".ts", ".tsx"] },
+	output: {
+		path: path.resolve(__dirname, "dist/"),
+		publicPath: "/dist/",
+		filename: "bundle.js",
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "public/"),
+		},
+		port: 3000,
+		hot: true,
+	},
+	plugins: [new webpack.HotModuleReplacementPlugin()],
+};
+`;
+
+const BABEL = `{
+    "presets": [
+        "@babel/env",
+        "@babel/preset-react"
+    ]
+}`;
+
+const DEPENDENCIES = [...Object.keys(templateJs.dependencies)];
+
 function init() {
 	let projectName;
 	const program = new commander.Command("minimal-react")
@@ -60,6 +115,9 @@ function init() {
 			name: appName,
 			version: "0.1.0",
 			private: true,
+			scripts: {
+				dev: "webpack-dev-server --mode development",
+			}
 		};
 		fs.ensureDirSync(appName);
 		process.chdir(appName);
@@ -68,13 +126,12 @@ function init() {
 			JSON.stringify(packageJson, null, 4) + os.EOL
 		);
 
-		console.log(originalDirectory);
-		return build(appName, originalDirectory, root);
+		return buildFiles(root);
 	}
 
-	function build(appName, originalDirectory, root) {
+	function buildFiles(root) {
 		const top = root;
-		console.log(HTML_BASE);
+
 		fs.mkdirSync(path.join(top, "/public"));
 		console.log(`${chalk.blue("Creating")} ${chalk.red("document")}.`);
 		process.chdir("public");
@@ -91,6 +148,31 @@ function init() {
 		console.log();
 		fs.writeFileSync("./index.tsx", INDEX_BASE);
 		console.log(`${chalk.green("Created index.tsx in /src")}.`);
+
+		const packageInstall = new Promise((resolve, reject) => {
+			DEPENDENCIES.forEach((dependency) => {
+				console.log(chalk.green(`Installing ${dependency}`));
+				installPackage(dependency);
+			}, resolve());
+		});
+		packageInstall.then(() => {
+			console.log(`${chalk.green("Installed all dependencies")}.`);
+		});
+		process.chdir(root);
+		fs.writeFileSync("./webpack.config.js", WEBPACK);
+		console.log(chalk.green("Created webpack.config.js"));
+		fs.writeFileSync("./.babelrc", BABEL);
+		console.log(chalk.green("Created .babelrc"));
+		console.log(
+			chalk.greenBright(
+				`Successfully created a new React app. Run cd ${projectName} npm run dev to start`
+			)
+		);
+	}
+
+	function installPackage(dependency) {
+		const status = execSync("npm install " + dependency, { encoding: "utf-8" });
+		console.log("installed", dependency);
 	}
 
 	function getTemporaryDirectory() {
